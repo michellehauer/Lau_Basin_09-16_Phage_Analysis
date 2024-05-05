50a83a149a0cdb192ded5ac0194e8627ee8d590d
Michelle Hauer <77941993+michellehauer@users.noreply.github.com>
2024-05-05 13:28:25 -0400

    Update phage_pipeline.sh

--- metagenomes/phage_pipeline.sh
+++ metagenomes/phage_pipeline.sh
@@ -56,3 +56,29 @@ source activate drep
 dRep dereplicate --S_algorithm ANImf --SkipMash -nc .5 -sa .95 -l 10000 -N50W 0 -sizeW 1 --ignoreGenomeQuality --clusterAlg single metagenome_drep_output -g {path_to_phage_contigs}/*fna
 conda deactivate
 
+### Run iPhop to predict the phage hosts
+conda activate iphop_env
+mkdir iphop_db
+cd iphop_db
+#download the db
+wget https://portal.nersc.gov/cfs/m342/iphop/db/iPHoP.latest_rw.tar.gz
+tar -zxvf iPHoP.latest_rw.tar.gz
+#check db integrity
+iphop download --db_dir iphop_db/Aug_2023_pub_rw/ --full_verify
+conda deactivate iphop_env
+
+# prepare to add my symbiont MAGs to the db
+conda activate /project/pi_rbeinart_uri_edu/michelle/conda/gtdb_2.1.1 
+conda env config vars set GTDBTK_DATA_PATH="release214"
+# add my symbiont mags (added 09-22 MAGs)
+gtdbtk de_novo_wf --genome_dir /project/pi_rbeinart_uri_edu/michelle/iphop/symbiont_MAGs --bacteria --outgroup_taxon p__Patescibacteria --out_dir symbiont_MAGs_GTDB-tk_results2/ --cpus 32 --force --extension fasta
+conda deactivate
+## there is a bug in the version of iphop so i had to manually change the ar53 file  in iphop_db2/Aug_2023_pub_rw/db_infos/), cp gtdbtk.ar53.decorated.tree gtdbtk.ar122.decorated.tree”, https://bitbucket.org/srouxjgi/iphop/issues/98/filenotfounderror-errno-2-no-such-file-or is the iphop issue discussion
+conda activate iphop_env
+iphop add_to_db --fna_dir symbiont_MAGs/ --gtdb_dir symbiont_MAGs_GTDB-tk_results/ --out_dir April_2024_pub_rw_w_MAG_hosts --db_dir /project/pi_rbeinart_uri_edu/michelle/iphop/iphop_db2/Aug_2023_pub_rw
+cd phage_contigs
+python3 add_filenames_to_headers.py .
+cat *fna > ../cat_phage.fasta
+cd ../
+# run iphop
+iphop predict --fa_file cat_phage.fasta --db_dir April_2024_pub_rw_w_MAG_hosts/ --out_dir iphop_output/ -t 4
